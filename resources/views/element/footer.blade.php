589f803e3350f33f5a6b4a5ba86bb0d3b4c485e3
Corentin <48736362+Eowalim@users.noreply.github.com>
2020-12-15 10:30:05 +0100

    Update footer.blade.php

--- resources/views/element/footer.blade.php
+++ resources/views/element/footer.blade.php
@@ -14,7 +14,7 @@
         <div class="row-cols-4">
             <div class="col-lg-auto h-auto text-center">
                 <p class="text-muted text-center small mb-4 mb-lg-0">© Loki 2020. Tous droits réservés.</p>
-                <p class="text-muted text-center small mb-4 mb-lg-0">Dupont Corentin & Lacroix Owen.</p>
+                <p class="text-muted text-center small mb-4 mb-lg-0">Corentin & Owen.</p>
             </div>
         </div>
     </div>
